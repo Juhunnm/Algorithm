2551e7b7e35c614af2f9a75d7042f15df3ffe8bd
juhun <minjuhun1@gmail.com>
2023-11-29 09:51:04 +0900

    binaryTest

---
+++ binary.cpp
@@ -0,0 +1,31 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include <stdio.h>
+//#include <time.h>
+//#include <stdlib.h>
+//#define MAX 1000
+//int main(void) {
+//    srand(time(NULL));
+//    int i = rand() % (MAX + 1);
+//    int num;
+//    int count = 0;
+//    printf("%d",i);
+//    while (1) {
+//        printf("숫자를 입력하세요: ");
+//        scanf("%d", &num);
+//        count += 1;
+//
+//        if (num == i) {
+//            printf("맞췄습니다.\n");
+//            break;
+//        }
+//        else if (num < i) {
+//            printf("더 큽니다.\n");
+//        }
+//        else if(num < i) {
+//            printf("더 작습니다.\n");
+//        }
+//    }
+//
+//    printf("시도 횟수: %d\n", count);
+//    return 0;
+//}
